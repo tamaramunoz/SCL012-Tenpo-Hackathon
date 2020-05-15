cffdefaa4d8199fa86d58685a0f8cfb7a538d63f
tamaramunoz <tami.skt@gmail.com>
2020-05-15 17:50:52 -0400

    agregue bootstrap y nube-dialogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Chat from './Views/Chat'
-
+import FloatButton from './components/FloatButton';
 
 
 function App() {
   return (
     <div>
+      <FloatButton />
       <Chat />
     </div>
   );
--- src/components/FloatButton.jsx
+++ src/components/FloatButton.jsx
@@ -1,12 +1,26 @@
 import React, { Fragment } from 'react'
-import '../index.css'
+import { DropdownButton, Dropdown } from 'react-bootstrap'
+import nube from '../img/nube-dialogo.png'
+
 
 const FloatButton = () => {
     return (
         <Fragment>
-            <div className="container" >
+            <img src={nube} alt="nube-dialogo" />  
+            <DropdownButton title='nubecita drop-down' >      
+                     
+                <Dropdown.Item eventKey="1">Posibles soluciones 1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Posibles soluciones 1</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item eventKey="4">Posibles soluciones 1</Dropdown.Item>
                 
-            </div>
+            </DropdownButton>
+            
+            
+             
+            
+            
+
         </Fragment>
     )
 }
