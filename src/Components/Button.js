a9b3e9cbfeefc40c36526979ded7beaab52b04ae
tamaramunoz <tami.skt@gmail.com>
2020-05-16 18:00:10 -0400

    nube de dialogo en boton

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/Button.css'
 
 const Button = ({ value, onClick }) => (
+    
     <button className='button-button'
         onClick={(event) => { onClick(event) }}>
         {value}
