f345834b7cdb479f973a81e567e6cb73041ec691
catalina-alzamora <56680334+catalina-alzamora@users.noreply.github.com>
2020-05-16 19:39:54 -0400

    agregando router y vista 1 (#5)
    * agregando componente de dialogo
    
    * vista1

---
+++ src/Views/V1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import v1 from '../img/1.png'
+import '../css/V1.css'
+
+
+const V1 = () => (
+    <div className='V1-container'>
+        <img src={v1} alt='primera vista de aplicación' className='V1-background'></img>
+    </div>
+)
+
+export default V1;
+
