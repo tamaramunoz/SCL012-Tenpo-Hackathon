589309dff59e5778a2b90e206ca970a7f74ed2ee
catalina-alzamora <alzamoracatalina@gmail.com>
2020-05-17 10:12:13 -0400

    v1 y V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import V1 from './Views/V1';
+import V2 from './Views/V2';
 import Chat from './Views/Chat';
 import FloatButton from './components/FloatButton';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/' component={V1} />
+        <Route exact path='/V2' component={V2} />
         <Route exact path='/Chat' component={Chat} />
         <Route exact path='/FloatButton' component={FloatButton} />
       </Switch>
