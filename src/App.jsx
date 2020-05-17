6eaa9a30923fc9cb651af3143badfb4e8b4bf9d0
catalina-alzamora <56680334+catalina-alzamora@users.noreply.github.com>
2020-05-17 12:07:59 -0400

    vistas 1 y 2 (#6)
    * agregando componente de dialogo
    
    * vista1
    
    * v1 y V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import V1 from './Views/V1';
+import V2 from './Views/V2';
 import Chat from './Views/Chat';
 import FloatButton from './components/FloatButton';
 import Assistance from './components/Assistance'
@@ -12,6 +13,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/' component={V1} />
+        <Route exact path='/V2' component={V2} />
         <Route exact path='/Chat' component={Chat} />
         <Route exact path='/floatButton' component={FloatButton} />
         <Route exact path='/assistance' component={Assistance} />
