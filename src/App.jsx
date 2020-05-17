5f9dc70109a78a802426334387e224abc1ba0826
catalina-alzamora <56680334+catalina-alzamora@users.noreply.github.com>
2020-05-17 19:09:47 -0400

    Vistas 3 4 5 6 (#7)
    * agregando componente de dialogo
    
    * vista1
    
    * v1 y V2
    
    * adding circle images
    
    * vistas 3 4 y 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import V1 from './Views/V1';
 import V2 from './Views/V2';
+import V3 from './Views/V3';
+import V4 from './Views/V4';
+import V5 from './Views/V5';
+import V6 from './Views/V6';
 import Chat from './Views/Chat';
 import FloatButton from './components/FloatButton';
 import Assistance from './components/Assistance'
@@ -14,6 +18,10 @@ function App() {
       <Switch>
         <Route exact path='/' component={V1} />
         <Route exact path='/V2' component={V2} />
+        <Route exact path='/V3' component={V3} />
+        <Route exact path='/V4' component={V4} />
+        <Route exact path='/V5' component={V5} />
+        <Route exact path='/V6' component={V6} />
         <Route exact path='/Chat' component={Chat} />
         <Route exact path='/floatButton' component={FloatButton} />
         <Route exact path='/assistance' component={Assistance} />
