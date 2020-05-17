641f187217aa8d99e5dcbe72ec2ca023345eab20
tamaramunoz <tami.skt@gmail.com>
2020-05-16 23:02:47 -0400

    integrando componente assistance al chatbot, creando teclado del telefono

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import V1 from './Views/V1';
 import Chat from './Views/Chat';
 import FloatButton from './components/FloatButton';
-import ModalMail from './components/ModalMail'
+import Assistance from './components/Assistance'
+import Phone from './components/Phone';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -11,8 +13,9 @@ function App() {
       <Switch>
         <Route exact path='/' component={V1} />
         <Route exact path='/Chat' component={Chat} />
-        <Route exact path='/FloatButton' component={FloatButton} />
-        <Route exact path='/ModalMail' component={ModalMail} />
+        <Route exact path='/floatButton' component={FloatButton} />
+        <Route exact path='/assistance' component={Assistance} />
+        <Route exact path='/phone' component={Phone} />
       </Switch>
     </Router>
   );
---
+++ src/Components/Assistance.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import { Dropdown } from 'react-bootstrap'
+import nube from '../img/nube-dialogo.png';
+import ModalMail from './ModalMail';
+
+const Assistance = () => {
+    return (
+        <Fragment>
+            <Dropdown>
+                <Dropdown.Toggle className="bg-light" >
+                    <img src={nube}
+                    alt="tenpo img"
+                    width={50}
+                    />
+                </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <ModalMail />
+            </Dropdown.Menu>
+        </Dropdown>
+
+        </Fragment>
+
+    )
+}
+
+export default Assistance;
