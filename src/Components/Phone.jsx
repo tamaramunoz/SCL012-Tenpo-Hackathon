b63f026ca24edf57884ccd8873ce0a7854ed1c14
tamaramunoz <tami.skt@gmail.com>
2020-05-17 14:14:47 -0400

    componente phone con sus estilos y responsive

--- src/Components/Phone.jsx
+++ src/Components/Phone.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import '../css/Phone.css'
+import phone from '../img/phone.png'
+import camera from '../img/camera.png'
+import keyboard from '../img/keyboard.png'
 
 const Phone = () => {
     return (
@@ -12,38 +16,38 @@ const Phone = () => {
                 <div className="options">
                     <p>Registro</p>
                     <p>Favoritos</p>
-                    <p>Contacto</p>
+                    <p>Contactos</p>
                 </div>
             </div>
             <div className="visor">
                 <p>600 360 0355</p>
             </div>
             <div className="numbers" >
-                <table>
-                    <tr>
-                        <th>1</th>
-                        <th>2</th>
-                        <th>3</th>
+                <table className="numb-group" >
+                    <tr className="numb" >
+                        <th><button>1</button></th>
+                        <th><button>2</button></th>
+                        <th><button>3</button></th>
                     </tr>
-                    <tr>
-                        <th>4</th>
-                        <th>5</th>
-                        <th>6</th>
+                    <tr className="numb" >
+                        <th><button>4</button></th>
+                        <th><button>5</button></th>
+                        <th><button>6</button></th>
                     </tr>
-                    <tr>
-                        <th>7</th>
-                        <th>8</th>
-                        <th>9</th>
+                    <tr className="numb" >
+                        <th><button>7</button></th>
+                        <th><button>8</button></th>
+                        <th><button>9</button></th>
                     </tr>
-                    <tr>
-                        <th>*</th>
-                        <th>0</th>
-                        <th>#</th>
+                    <tr className="numb" >
+                        <th><button>*</button></th>
+                        <th><button>0</button></th>
+                        <th><button>#</button></th>
                     </tr>
-                    <tr>
-                        <th>cam</th>
-                        <th>call</th>
-                        <th>hide</th>
+                    <tr className="icons-group">
+                        <th><img src={camera} alt="video llamada" width={35} /></th>
+                        <th><Link to="/assistance"><img src={phone} alt="llamar" width={60} /></Link></th>
+                        <th><img src={keyboard} alt="ocultar teclado" width={35} /></th>
                     </tr>
                 </table>
             </div>
