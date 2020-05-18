27d4cd05ec598c8682e30ef0ffd71830b4afedc4
tamaramunoz <tami.skt@gmail.com>
2020-05-18 17:40:02 -0400

    agregando componente y css de addphone

--- src/Views/AddPhone.jsx
+++ src/Views/AddPhone.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import '../css/AddPhone.css'
 import back from '../img/back.png'
 
 const AddPhone = () => {
     return (
-        <div>
-            <div>
-                <img src={back} alt="Volver atras"/>
+        <div className="addphone-container" >
+            <div className="addphone-header" >
+                <img src={back} alt="Volver atras" />
                 <p>¡VAMOS NO FALTA NADA!</p>
             </div>
-            <div>
-                <p>Ingresa tu celular</p>
+            <div className="addphone-body" >
+                <p className="addphone-body-title"  >Ingresa tu celular</p>
                 <p>Te enviaremos un mensaje con un código para validar tu número.</p>
-                <input type="text"/>
+            </div>
+            <div className="addphone-inputs"  >
+                <input type="text" placeholder="+56 | Ingresa tu número" />
+            </div>
+            <div className="addphone-button" >
                 <button>Validar por Whatsapp</button>
                 <button>Validar por SMS</button>
             </div>
--- src/Views/EnterAddress.jsx
+++ src/Views/EnterAddress.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import '../css/Address.css'
 import back from '../img/back.png'
+
 
 const EnterAddress = () => {
     return (
-        <div>
-            <div>
-                <img src={back} alt="" />
+        <div className="address-container" >
+            <div className="address-header" >
+                <img src={back} alt="volver atras" />
                 <p>¡Un poco más y estamos listos!</p>
             </div>
-            <div>
-                <p>Ingresa tu dirección</p>
+            <div className="address-body" >
+                <p className="address-body-title" >Ingresa tu dirección</p>
                 <p>Todos tus datos son confidenciales</p>
             </div>
-            <div>
+            <div className="address-inputs" >
                 <input type="text" placeholder="Región" />
                 <input type="text" placeholder="Comuna" />
                 <input type="text" placeholder="Dirección" />
             </div>
-            <div>
+            <div className="address-button" >
                 <button>Continuar</button>
             </div>
         </div>
