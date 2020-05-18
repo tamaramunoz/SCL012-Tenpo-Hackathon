e0de13fc54503c9b4dac46d034e233d18ab9b4d8
tamaramunoz <tami.skt@gmail.com>
2020-05-18 13:04:24 -0400

    agregando informacion de componente enteraddress

--- src/Views/EnterAddress.jsx
+++ src/Views/EnterAddress.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import back from '../img/back.png'
 
 const EnterAddress = () => {
     return (
         <div>
-            
+            <div>
+                <img src={back} alt="" />
+                <p>¡Un poco más y estamos listos!</p>
+            </div>
+            <div>
+                <p>Ingresa tu dirección</p>
+                <p>Todos tus datos son confidenciales</p>
+            </div>
+            <div>
+                <input type="text" placeholder="Región" />
+                <input type="text" placeholder="Comuna" />
+                <input type="text" placeholder="Dirección" />
+            </div>
+            <div>
+                <button>Continuar</button>
+            </div>
         </div>
     )
 }
