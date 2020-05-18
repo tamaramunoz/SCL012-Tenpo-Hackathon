1ac399ea4b11ddc05703ca473c9239bd9942f36a
Camila Rojas S <56646322+Camirojasserantoni@users.noreply.github.com>
2020-05-18 17:41:48 -0400

    agregando  icono de V9  (#12)
    * subiendo boton desplegable
    
    * subiendo cambios para bajar actualizacion de cata
    
    * subiendo cambios para bajar 1
    
    * corrigiendo errores para tener cambios completos
    
    * coordinando vistas hasta la 8
    
    * agregando V9 sujeta a cambios
    
    * agregando V9, sujeta a cambios futuros
    
    * agregando acento en readme
    
    * subiendo icono de V9, falta arreglar detalle de V8
    
    Co-authored-by: Tamara Muñoz <56234189+tamaramunoz@users.noreply.github.com>

--- src/Views/V9.js
+++ src/Views/V9.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PTenV9 from '../img/PTenV9.png';
+import chatBot from '../img/information.png';
+import '../css/V9.css'
 
 
 const V9 = () => (
     < div className='V9-container'>
             <img src={PTenV9} alt='Pantalla tenpo 9'  className="pTenV9"></img>
+           
+            <img src={chatBot} alt='icono de información' className="infoV9"/>    
+            
     </div>
 )
 
