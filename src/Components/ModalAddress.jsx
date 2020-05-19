ff9c9b814d9a535883b99bd64494be6809b60632
tamaramunoz <tami.skt@gmail.com>
2020-05-18 22:11:38 -0400

    linkeando componentes modal address y validate

--- src/Components/ModalAddress.jsx
+++ src/Components/ModalAddress.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import '../css/ModalMail.css'
+import '../css/ModalAddress.css'
 import back from '../img/back.png'
 import PhoneKey from '../img/phone-key.png'
 
@@ -9,11 +9,11 @@ const ModalAddress = () => {
     return (
         <div className="container-modal" >
             <div>
-                <img src={back} alt="Volver atras" />
+                <Link to="/address" ><img src={back} alt="Volver atras" /></Link>
             </div>
-
-            <Link to="/address" ><img src={PhoneKey} alt="error al continuar" /></Link>
-            
+            <div className="container-imagen" >
+                <Link to="/address" ><img src={PhoneKey} alt="error al continuar" /></Link>
+            </div>
             <div className="text-box" >
                 <p>La Unidad de Análisis Financiero (UAF) nos exige conocer todos estos datos de los usuarios que se registren para crear una cuenta.</p>
                 <p>Esto es una medida de protección para <b>evitar fraudes y estafas</b>. </p>
