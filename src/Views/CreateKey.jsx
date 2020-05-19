6737c6a72b562a5b4f2722559ef6f238b57c3ade
tamaramunoz <tami.skt@gmail.com>
2020-05-18 20:42:18 -0400

    agregando estilos de los componentes create y confirm key

--- src/Views/CreateKey.jsx
+++ src/Views/CreateKey.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import back from '../img/back.png'
+import '../css/CreateKey.css'
 import { Link } from 'react-router-dom';
 
 const CreateKey = () => {
     return (
-        <div>
-            <div>
-                <Link to="address" ><img src={back} alt="Volver atras"/></Link>
+        <div className="key-container" >
+            <div className="key-header">
+                <Link to="/address" ><img src={back} alt="Volver atras"/></Link>
                 <p>¡Y por último! Tu seguridad</p>
             </div>
-            <div>
-                <p>Crea tu clave de accesos de 4 dígitos</p>
+            <div className="key-body">
+                <p className="key-body-title" >Crea tu clave de accesos de 4 dígitos</p>
                 <p>Con ella podrás ingresar de forma segura.</p>
             </div>
-            <div>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
+            <div className="key-inputs">
+                <input type="password" maxLength="1" />
+                <input type="password" maxLength="1" />
+                <input type="password" maxLength="1" />
+                <input type="password" maxLength="1" />
             </div>
-            <div>
+            <div className="key-button">
                <Link to="/confirmkey"><button>Continuar</button></Link>
             </div>
             
